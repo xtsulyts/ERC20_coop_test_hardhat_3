f5b9dba2e60fd4b44ef1997f3d270ad1714226da
xtsulyts <531xtsulyts@gmail.com>
2025-10-26 11:14:29 -0300

    seteo llaves privadas en hardhat.config con hardhat-keystore.

--- backend/hardhat.config.ts
+++ backend/hardhat.config.ts
@@ -2,10 +2,16 @@ import type { HardhatUserConfig } from "hardhat/config";
 import hardhatToolboxViemPlugin from "@nomicfoundation/hardhat-toolbox-viem";
 import { configVariable } from "hardhat/config";
 import hardhatViem from "@nomicfoundation/hardhat-viem";
-import hardhatVerify from "@nomicfoundation/hardhat-verify"
+import hardhatVerify from "@nomicfoundation/hardhat-verify";
+import hardhatKeystore from "@nomicfoundation/hardhat-keystore"
 
 const config: HardhatUserConfig = {
-  plugins: [hardhatToolboxViemPlugin, hardhatViem, hardhatVerify],
+  plugins:
+   [hardhatToolboxViemPlugin, 
+    hardhatViem, 
+    hardhatVerify, 
+    hardhatKeystore
+  ],
   solidity: {
     profiles: {
       default: {

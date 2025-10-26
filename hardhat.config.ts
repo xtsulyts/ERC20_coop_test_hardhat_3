336a539346c6f567da31d46b6cba3a4b37629317
xtsulyts <531xtsulyts@gmail.com>
2025-10-26 10:16:24 -0300

    instalo dependencias @nomicfoundation/hardhat-verify y @nomicfoundation/hardhat-keystore.

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -2,9 +2,10 @@ import type { HardhatUserConfig } from "hardhat/config";
 import hardhatToolboxViemPlugin from "@nomicfoundation/hardhat-toolbox-viem";
 import { configVariable } from "hardhat/config";
 import hardhatViem from "@nomicfoundation/hardhat-viem";
+import hardhatVerify from "@nomicfoundation/hardhat-verify"
 
 const config: HardhatUserConfig = {
-  plugins: [hardhatToolboxViemPlugin, hardhatViem],
+  plugins: [hardhatToolboxViemPlugin, hardhatViem, hardhatVerify],
   solidity: {
     profiles: {
       default: {
@@ -38,6 +39,13 @@ const config: HardhatUserConfig = {
       accounts: [configVariable("SEPOLIA_PRIVATE_KEY")],
     },
   },
+  verify: {
+    etherscan: {
+      // Your API key for Etherscan
+      // Obtain one at https://etherscan.io/
+      apiKey: configVariable("ETHERSCAN_API_KEY"),
+    },
+  },   
 };
 
 export default config;
